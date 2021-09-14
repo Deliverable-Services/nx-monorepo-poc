a54d0300ff8fb298e774310f80725645ca5eb834
Dishant Agnihotri <dishantagnihotri@gmail.com>
2021-09-15 01:13:34 +0530

    Base ready

--- apps/client-portal/src/main.tsx
+++ apps/client-portal/src/main.tsx
@@ -1,11 +1,32 @@
-import { StrictMode } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import * as ReactDOM from 'react-dom';
 
+import { Feature as BookFeature } from '@cea/feature';
 import App from './app/app';
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Route
+      path="/"
+      exact
+      render={() => (
+        <div>
+          Chapter 2: Libraries 20 This is the generated root route.{' '}
+          <Link to="/page-2">Click here for page 2.</Link>
+          <Link to="/feature">Click here for feature.</Link>
+        </div>
+      )}
+    />
+    <Route path="/feature" component={BookFeature} />
+    <Route
+      path="/page-2"
+      exact
+      render={() => (
+        <div>
+          <Link to="/">Click here to go back to root page.</Link>
+        </div>
+      )}
+    />
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ libs/feature/src/lib/feature.tsx
@@ -0,0 +1,14 @@
+import './feature.module.scss';
+
+/* eslint-disable-next-line */
+export interface FeatureProps {}
+
+export function Feature(props: FeatureProps) {
+  return (
+    <div>
+      <h1>Welcome to Feature!</h1>
+    </div>
+  );
+}
+
+export default Feature;
